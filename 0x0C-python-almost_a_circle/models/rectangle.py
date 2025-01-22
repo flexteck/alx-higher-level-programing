77491ef9a0da850d3d5ca2c29b9935ae7bd76f04
flexteck <flexteckse@gmail.com>
2025-01-22 14:21:51 +0100

    added a public method 'to_dictionary() to the Rectangle class that returns a dictionary representation of a rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -101,3 +101,14 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 if hasattr(self, key):
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ return the dictionary representation of a rectangle """
+
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+            }
