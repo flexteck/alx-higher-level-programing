b08426315513e44e0df1bbc19b229b6cc1fa62e7
flexteck <flexteckse@gmail.com>
2025-01-22 14:41:37 +0100

    updated Base class by adding static method 'to_json_string(list_dictionaries)' that returns a json representation of 'list_dictionaries'

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -42,3 +42,8 @@ class Square(Rectangle):
 			for key, value in kwargs.items():
 				if hasattr(self, key):
 					setattr(self, key, value)
+
+	def to_dictionary(self):
+		""" returns a dict rep of a Square"""
+
+		return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
