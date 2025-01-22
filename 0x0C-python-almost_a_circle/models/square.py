a5c9eb2920ceca2a8435d014bfb495a1fd49eeb3
flexteck <flexteckse@gmail.com>
2025-01-22 13:32:57 +0100

    added update method to Square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,4 +8,37 @@ class Square(Rectangle):
 		
 
 	def __str__(self):
-		return f"[Square] ({self.id} {self.x} / {self.y} - {self.height})"
+		return f"[Square] ({self.id}) {self.x} / {self.y} - {self.height}"
+
+
+	@property
+	def size(self):
+		return self.width
+
+	@size.setter
+	def size(self, value):
+		self.width = value
+		self.height = value
+
+	def update(self, *args, **kwargs):
+		"""
+		assigns attributes using args or kwargs.
+		*args (positional argument):
+			1st argument : id
+			2nd argument : size
+			3rd argument : x
+			4th argument : y
+
+		**kwargs: non positional arguments.
+		"""
+
+
+		if args and len(args) > 0:
+			attributes = ["id", "size", "x", "y"]
+			for i, value in enumerate(args):
+				if i < len(attributes):
+					setattr(self, attributes[i], value)
+		elif kwargs:
+			for key, value in kwargs.items():
+				if hasattr(self, key):
+					setattr(self, key, value)
