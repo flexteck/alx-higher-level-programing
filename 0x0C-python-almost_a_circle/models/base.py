e88cabf807568689faffbc3893ef2339ffcf894d
flexteck <flexteckse@gmail.com>
2025-01-24 15:00:54 +0100

    added two methods def create(cls, **dictionary): and from_json_string(json_string) in the Base class that that create a new instance of a class and desrialize a json text respectively

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,3 +19,37 @@ class Base:
             return "[]"
 
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        
+        filename = f"{cls.__name__}.json"
+
+        with open(filename, "w", encoding="utf-8") as file:
+            if list_objs is None or len(list_objs) == 0:
+                file.write("[]")
+            else:
+                list_dict = [obj.to_dictionary() for obj in list_objs]
+                json_string = cls.to_json_string(list_dict)
+                file.write(json_string)
+
+    
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string == None or len(json_string) == 0:
+            return "[]"
+
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            raise ValueError("Unsupported class ")
+
+        dummy.update(**dictionary)
+
+        return dummy
